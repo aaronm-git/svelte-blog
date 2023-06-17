25484054b9de45fcefd926d3d1069a5462bd0c6f
git_repo_user <evalgit@example.com>
2023-06-17 15:27:51 -0400

    yahoo's pure css blog created with sveltekit

---
+++ src/lib/components/PageContainer.svelte
@@ -0,0 +1,3 @@
+<div class="content pure-u-1 pure-u-md-3-4">
+<slot></slot>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import PageContainer from '$lib/components/PageContainer.svelte';
+</script>
+
+<svelte:head>
+	<title>Blog &ndash; Layout Examples &ndash; Pure</title>
+	<meta name="description" content="A layout example that shows off a blog page with a list of posts.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/css/pure-min.css">
+	<link rel="stylesheet" href="/css/grids-responsive.css">
+	<link rel="stylesheet" href="/css/styles.css">
+</svelte:head>
+
+
+
+<div id="layout" class="pure-g">
+	<Sidebar />
+	<PageContainer>
+		<slot />
+	</PageContainer>
+</div>
