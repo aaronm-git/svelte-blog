9c15688fc6b07ad34e38cd24cac41743afb8e170
git_repo_user <evalgit@example.com>
2023-06-22 21:21:43 -0400

    error handling

---
+++ src/lib/constants/error.js
@@ -0,0 +1,8 @@
+// export ERROR_CODE_ENUMS with message.
+export const ERROR_CODE_ENUMS = {
+    NOT_FOUND: "Not found",
+    FORBIDDEN: "Forbidden",
+    UNAUTHORIZED: "Unauthorized",
+    BAD_REQUEST: "Bad request",
+    INTERNAL_SERVER_ERROR: "Internal server error"
+}
---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { ERROR_CODE_ENUMS } from '$lib/constants/error';
+	import { page } from '$app/stores';
+	const errorCode = $page.error.code;
+	const errorMessage = ERROR_CODE_ENUMS[errorCode];
+</script>
+
+<div>
+	<h1>{$page.status}</h1>
+	<p>{errorMessage}</p>
+	<a href="/" class="pure-button">Back to Home</a>
+</div>
+
+<style>
+	* {
+		text-align: center;
+	}
+
+	h1 {
+		color: lightgray;
+		font-size: 10rem;
+		margin: 25% 0 0 0;
+	}
+
+	p {
+		color: lightgray;
+		font-size: 2rem;
+		margin: 0;
+	}
+
+	a {
+		margin-top: 1rem;
+        text-decoration: none;
+	}
+</style>
